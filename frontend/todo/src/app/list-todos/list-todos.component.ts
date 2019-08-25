bc802bc79835f6d4ceb24e60c84a9c61763cb1be
Arya <arya.atighehchian@gmail.com>
2019-08-25 13:23:54 -0400

    added delete component

--- frontend/todo/src/app/list-todos/list-todos.component.ts
+++ frontend/todo/src/app/list-todos/list-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo {
   constructor(
@@ -16,22 +17,47 @@ export class Todo {
   styleUrls: ['./list-todos.component.css']
 })
 export class ListTodosComponent implements OnInit {
-  todos = [
-    new Todo(1,'Learn to dance', false, new Date()),
-    new Todo(2,'Become an expert at Angular', false, new Date()),
-    new Todo(3,'Visit California', false, new Date())
+  todos : Todo[];
+  message: String
+
+    // new Todo(1,'Learn to dance', false, new Date()),
+    // new Todo(2,'Become an expert at Angular', false, new Date()),
+    // new Todo(3,'Visit California', false, new Date())
 
     // {id : 1, description : 'Learn to dance'},
     // {id : 2, description : 'Become an expert at Angular'},
     // {id : 3, description : 'Visit California'}
-  ]
+  
   // todo = {
   //   id : 1,
   //   description : 'Learn to dance'
   // }
-  constructor() { }
+  constructor(
+    private todoService: TodoDataService
+  ) { }
 
   ngOnInit() {
+    this.refreshTodos();
+  }
+
+  refreshTodos(){
+    this.todoService.retrieveAllTodos('Arya').subscribe(
+      response => {
+        console.log(response);
+        this.todos = response;
+      }
+    )
+  }
+
+  deleteTodo(id){
+    console.log(`delete todo ${id}`);
+    this.todoService.deleteTodo('arya',id).subscribe(
+      response => {
+        console.log(response);
+        this.message = `Delete successful of Todo ${id}`;
+        this.refreshTodos();
+      }
+    );
   }
 
 }
